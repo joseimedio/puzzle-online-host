84876ecd1edddae422f1a69dca45a28dc1f7b478
Jose Imedio <jose.adan.imedio@gmail.com>
2023-03-16 13:07:53 +0100

    Port change

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
     user:     local ? dbLocal.user     : db.user,
     password: local ? dbLocal.password : db.password,
     host:     local ? dbLocal.host     : db.host,
-    port:     local ? dbLocal.port     : db.port,
+    port:     local ? dbLocal.port     : process.env.PORT,
     database: local ? dbLocal.database : db.database,
     ssl: {
         rejectUnauthorized: false
