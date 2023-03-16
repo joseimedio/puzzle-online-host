a07e0be86511ffc3ef80c2b5675f27ad37c97077
Jose Imedio <jose.adan.imedio@gmail.com>
2023-03-16 14:52:14 +0100

    Add connection string

--- src/db.js
+++ src/db.js
@@ -3,12 +3,20 @@ const { db, dbLocal } = require('./config')
 
 const local = false;
 
+// const pool = new Pool({
+//     user:     local ? dbLocal.user     : db.user,
+//     password: local ? dbLocal.password : db.password,
+//     host:     local ? dbLocal.host     : db.host,
+//     port:     local ? dbLocal.port     : db.port,
+//     database: local ? dbLocal.database : db.database,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
+
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
-    user:     local ? dbLocal.user     : db.user,
-    password: local ? dbLocal.password : db.password,
-    host:     local ? dbLocal.host     : db.host,
-    port:     local ? dbLocal.port     : db.port,
-    database: local ? dbLocal.database : db.database,
+    connectionString,
     ssl: {
         rejectUnauthorized: false
     }
